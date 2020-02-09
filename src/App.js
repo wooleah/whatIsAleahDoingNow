813f1ce037ccf6b4b611ef13a954f035ea487888
wooleah <woojaeizzle@gmail.com>
2020-02-09 22:21:21 +0900

    Added custom font

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function whatIsSheDoing(verb, noun) {
 
 function App() {
   return (
-    <div className="App dt vh-100 w-100 bg-light-gray">
+    <div className="App dt vh-100 w-100 bg-light-yellow">
       <div className="App__context dtc tc v-mid dib br3 pa3 ma2 grow">
         <div>
           <h1>Aleah is</h1>
-          <h1>{whatIsSheDoing('Singing', 'I believe I can fly')}</h1>
-          <h1>Why? I'm not sure :p</h1>
+          <h1>{whatIsSheDoing('Playing', 'Zelda')}</h1>
+          <h1>24 hours a day like crazy</h1>
           <div className="App__image"></div>
         </div>
         <img src={logo} className="App__logo" alt="Wooleah logo"></img>
