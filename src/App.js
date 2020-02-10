9f5f003ea441459f90a5873cc4a3d9bc2bd1825e
wooleah <woojaeizzle@gmail.com>
2020-02-11 07:34:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <div className="App__context dtc tc v-mid dib br3 pa3 ma2 grow font-effect-shadow-multiple">
         <div>
           <h1>Aleah is</h1>
-          <h1>{whatIsSheDoing('Sleeping', 'Safe and Sound')}</h1>
-          <h1>By Capital Cities</h1>
+          <h1>{whatIsSheDoing('Dreaming', 'about eating bulgogi')}</h1>
+          <h1>But can't because she woke up</h1>
           <div className="App__image"></div>
         </div>
         <img src={logo} className="App__logo" alt="Wooleah logo"></img>
